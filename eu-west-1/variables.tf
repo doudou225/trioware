e37b1273d5ec8af015646558b48801a7dc08811e
Abdou Ndiaye <andiaye@suvoda.com>
2025-03-20 19:05:49 -0400

    Version 1.0.0

--- eu-west-1/variables.tf
+++ eu-west-1/variables.tf
@@ -20,6 +20,11 @@ variable "path_r7_deb_linux_x64" {
 
 variable "r7_linux_version" {
   type = string
+  
+  validation {
+    condition = can(regex("^[0-9]+(\\.[0-9]+)*$", var.r7_linux_version))
+    error_message = "Rapid7 version consists only of digits and dots!"
+  }
 }
 
 variable "path_pmp_windows" {
